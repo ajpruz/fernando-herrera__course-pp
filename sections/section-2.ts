c8781e8e06269d1d99a1fe8ceac88aed8d759d9b
ajpruz <alexisjpruz@gmail.com>
2023-09-17 19:07:59 -0400

    section#2

---
+++ sections/section-2.ts
@@ -0,0 +1,26 @@
+let numberOfLines = 1;
+
+export function printLineNumber() {
+  console.log("Linea #", numberOfLines++);
+  //  numberOfLines = numberOfLines + 3;
+}
+
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
+
+/* 
+console.log(`linea #`, numberOfLines);
+//TODO:
+//numberOfLines++;
+increaseLineNumber();
+console.log(`linea #`, numberOfLines);
+//numberOfLines += 1;
+
+increaseLineNumber();
+console.log(`linea #`, numberOfLines);
+//numberOfLines = numberOfLines + 1;
+increaseLineNumber();
+console.log(`linea #`, numberOfLines);
+ */
